2f8a484326d16c030e0e9bba768faa4592538727
Darshit Joshi <92263919+darshitjoshi@users.noreply.github.com>
2022-12-02 00:50:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+document.addEventListener("keydown", function (event) {
+  var btn = event.key;
+  sound(btn);
+  animation(btn);
+});
+
+var drumButton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < drumButton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var btn = this.innerHTML;
+    sound(btn);
+    animation(btn);
+  });
+}
+
+
+function animation(btn)
+{
+    var activeBtn = document.querySelector("."+btn);
+    activeBtn.classList.add("pressed");
+    setTimeout(function()
+    {
+        activeBtn.classList.remove("pressed");
+    },100);
+}
+
+
+
+function sound(btn) {
+  var w = new Audio("sounds/crash.mp3");
+  var a = new Audio("sounds/snare.mp3");
+  var s = new Audio("sounds/kick-bass.mp3");
+  var d = new Audio("sounds/tom-1.mp3");
+  var j = new Audio("sounds/tom-2.mp3");
+  var k = new Audio("sounds/tom-3.mp3");
+  var l = new Audio("sounds/tom-4.mp3");
+
+  switch (btn) {
+    case "w":
+      w.play();
+      break;
+    case "a":
+      a.play();
+      break;
+    case "s":
+      s.play();
+      break;
+    case "d":
+      d.play();
+      break;
+    case "j":
+      j.play();
+      break;
+    case "k":
+      k.play();
+      break;
+    case "l":
+      l.play();
+      break;
+    default:
+      break;
+  }
+}
+
+
+
+// document.querySelector("button").addEventListener("click",function()
+// {
+// var audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
+// });
+
+// function handleClick() {
+//
+// }
